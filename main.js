5122bff5fba8466f9cb3933c745807e487361bf0
MediukIV <143748068+MediukIV@users.noreply.github.com>
2024-04-08 22:03:46 +0300

    add_js

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// Модалка
-(() => {
-  const refs = {
-    openModalBtn: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.forEach(element => {
-    element.addEventListener('click', toggleModal);
-  });
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  //* Закривання по кліку на бекдроп
-
-  refs.modal.addEventListener('click', removeModal);
-  function removeModal(e) {
-    if (e.target === e.currentTarget) {
-      refs.modal.classList.add('is-hidden');
-    }
-  }
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
-  }
-})();
-
 // Mob-menu
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
@@ -55,4 +27,32 @@
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
+})();
+
+// Модалка
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.forEach(element => {
+    element.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  //* Закривання по кліку на бекдроп
+
+  refs.modal.addEventListener('click', removeModal);
+  function removeModal(e) {
+    if (e.target === e.currentTarget) {
+      refs.modal.classList.add('is-hidden');
+    }
+  }
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
 })();
